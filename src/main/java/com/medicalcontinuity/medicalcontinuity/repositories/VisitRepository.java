df557d07012983aef1ee90896a4c31d914e879f0
udayps10 <uday2006is@gmail.com>
2026-08-24 01:11:21 +0530

    Move VisitRepository to repositories package

--- src/main/java/com/medicalcontinuity/medicalcontinuity/repositories/VisitRepository.java
+++ src/main/java/com/medicalcontinuity/medicalcontinuity/repositories/VisitRepository.java
@@ -1,4 +1,4 @@
-package com.medicalcontinuity.medicalcontinuity.repository;
+package com.medicalcontinuity.medicalcontinuity.repositories;
 
 import com.medicalcontinuity.medicalcontinuity.entity.Visit;
 import org.springframework.data.jpa.repository.JpaRepository;
