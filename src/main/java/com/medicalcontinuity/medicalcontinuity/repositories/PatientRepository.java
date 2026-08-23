fdc16a1a5483c79173805ee4631d125f9396645f
udayps10 <uday2006is@gmail.com>
2026-08-24 01:10:34 +0530

    Move PatientRepository to repositories package

--- src/main/java/com/medicalcontinuity/medicalcontinuity/repositories/PatientRepository.java
+++ src/main/java/com/medicalcontinuity/medicalcontinuity/repositories/PatientRepository.java
@@ -1,4 +1,4 @@
-package com.medicalcontinuity.medicalcontinuity.repository;
+package com.medicalcontinuity.medicalcontinuity.repositories;
 
 import com.medicalcontinuity.medicalcontinuity.entity.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
