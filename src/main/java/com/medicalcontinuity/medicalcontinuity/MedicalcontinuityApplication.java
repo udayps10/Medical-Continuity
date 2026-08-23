a099380d8330725cdb4b59471dfe3ae64245be00
udayps10 <uday2006is@gmail.com>
2026-08-24 00:43:55 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/medicalcontinuity/medicalcontinuity/MedicalcontinuityApplication.java
@@ -0,0 +1,13 @@
+package com.medicalcontinuity.medicalcontinuity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalcontinuityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalcontinuityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medicalcontinuity/medicalcontinuity/MedicalcontinuityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicalcontinuity.medicalcontinuity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalcontinuityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
