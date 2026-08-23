9e65af247c6989be6287a21933c76e8090a51da1
udayps10 <uday2006is@gmail.com>
2026-08-24 00:55:59 +0530

    Add Hospital entity

---
+++ src/main/java/com/medicalcontinuity/medicalcontinuity/entity/Hospital.java
@@ -0,0 +1,34 @@
+package com.medicalcontinuity.medicalcontinuity.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "hospitals")
+public class Hospital {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String city;
+
+    public Hospital() {}
+
+    public Hospital(String name, String city) {
+        this.name = name;
+        this.city = city;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getCity() { return city; }
+    public void setCity(String city) { this.city = city; }
+}
