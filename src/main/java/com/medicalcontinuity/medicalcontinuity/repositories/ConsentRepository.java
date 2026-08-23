6151d42779df7d6f12c3bba55d669de7b68f0474
udayps10 <uday2006is@gmail.com>
2026-08-24 01:11:50 +0530

    Move ConsentRepository to repositories package

--- src/main/java/com/medicalcontinuity/medicalcontinuity/repositories/ConsentRepository.java
+++ src/main/java/com/medicalcontinuity/medicalcontinuity/repositories/ConsentRepository.java
@@ -1,4 +1,4 @@
-package com.medicalcontinuity.medicalcontinuity.repository;
+package com.medicalcontinuity.medicalcontinuity.repositories;
 
 import com.medicalcontinuity.medicalcontinuity.entity.Consent;
 import org.springframework.data.jpa.repository.JpaRepository;
