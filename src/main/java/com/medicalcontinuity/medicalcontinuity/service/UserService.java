5878ab02562f99c5df1da1de7d6f949dfe40b42d
udayps10 <uday2006is@gmail.com>
2026-08-24 23:40:54 +0530

    Add UserService

---
+++ src/main/java/com/medicalcontinuity/medicalcontinuity/service/UserService.java
@@ -0,0 +1,41 @@
+package com.medicalcontinuity.medicalcontinuity.service;
+
+import com.medicalcontinuity.medicalcontinuity.entity.User;
+import com.medicalcontinuity.medicalcontinuity.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User updateUser(Long id, User userDetails) {
+        User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+        user.setName(userDetails.getName());
+        user.setEmail(userDetails.getEmail());
+        user.setOtpVerified(userDetails.isOtpVerified());
+        user.setRole(userDetails.getRole());
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
