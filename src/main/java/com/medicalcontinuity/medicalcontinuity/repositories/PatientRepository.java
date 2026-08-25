8481b235882e647975c34d1d825fced951229271
udayps10 <uday2006is@gmail.com>
2026-08-25 09:36:01 +0530

    Add findByUserId to PatientRepository

--- src/main/java/com/medicalcontinuity/medicalcontinuity/repositories/PatientRepository.java
+++ src/main/java/com/medicalcontinuity/medicalcontinuity/repositories/PatientRepository.java
@@ -4,6 +4,9 @@ import com.medicalcontinuity.medicalcontinuity.entity.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Optional<Patient> findByUserId(Long userId);
 }
