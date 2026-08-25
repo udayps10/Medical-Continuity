48612de4ebaee9e99cdbd620890222f68756bb05
udayps10 <uday2006is@gmail.com>
2026-08-25 09:37:50 +0530

    Add findByVisitId to RecordRepository

--- src/main/java/com/medicalcontinuity/medicalcontinuity/repositories/RecordRepository.java
+++ src/main/java/com/medicalcontinuity/medicalcontinuity/repositories/RecordRepository.java
@@ -4,6 +4,9 @@ import com.medicalcontinuity.medicalcontinuity.entity.Record;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RecordRepository extends JpaRepository<Record, Long> {
+    List<Record> findByVisitId(Long visitId);
 }
