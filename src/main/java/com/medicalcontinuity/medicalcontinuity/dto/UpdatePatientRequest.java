649a18438b8892d971a6b94fb7cc813e06c5b048
Udaypratap Singh <uday2006is@gmail.com>
2026-08-30 10:00:08 -0700

    chore: remove UpdateRecordRequest.java

--- src/main/java/com/medicalcontinuity/medicalcontinuity/dto/UpdatePatientRequest.java
+++
@@ -1,8 +0,0 @@
-package com.medicalcontinuity.medicalcontinuity.dto;
-
-public class UpdatePatientRequest {
-    private String allergies;
-
-    public String getAllergies() { return allergies; }
-    public void setAllergies(String allergies) { this.allergies = allergies; }
-}
