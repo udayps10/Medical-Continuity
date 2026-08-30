74edae2fbebe7f14ab1a7ff2c0ddb36ea489f992
Udaypratap Singh <uday2006is@gmail.com>
2026-08-30 10:00:39 -0700

    chore: remove HospitalRepository.java

--- src/main/java/com/medicalcontinuity/medicalcontinuity/repositories/HospitalRepository.java
+++
@@ -1,9 +0,0 @@
-package com.medicalcontinuity.medicalcontinuity.repositories;
-
-import com.medicalcontinuity.medicalcontinuity.entity.Hospital;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HospitalRepository extends JpaRepository<Hospital, Long> {
-}
