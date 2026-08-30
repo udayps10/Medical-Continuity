6e829abe1b5f0a0af51607c80a0a5acf5aefebe7
Udaypratap Singh <uday2006is@gmail.com>
2026-08-30 15:09:16 -0700

    feat: add PatientRepository

---
+++ src/main/java/com/medicalcontinuity/medicalcontinuity/repositories/PatientRepository.java
@@ -0,0 +1,15 @@
+package com.medicalcontinuity.medicalcontinuity.repositories;
+
+import com.medicalcontinuity.medicalcontinuity.entity.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+
+    Optional<Patient> findByMcid(String mcid);
+
+    boolean existsByMcid(String mcid);
+}
