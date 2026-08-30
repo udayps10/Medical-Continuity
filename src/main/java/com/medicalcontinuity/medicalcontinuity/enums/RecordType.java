f5b6bc60352dfc1780f4ac8f7f49a4879961fe3c
Udaypratap Singh <uday2006is@gmail.com>
2026-08-30 10:04:42 -0700

    feat: add RecordType enum

---
+++ src/main/java/com/medicalcontinuity/medicalcontinuity/enums/RecordType.java
@@ -0,0 +1,13 @@
+package com.medicalcontinuity.medicalcontinuity.enums;
+
+public enum RecordType {
+    DIAGNOSIS,
+    PRESCRIPTION,
+    LAB_RESULT,
+    SURGERY,
+    VACCINATION,
+    ALLERGY,
+    RADIOLOGY,
+    DISCHARGE_SUMMARY,
+    OTHER
+}
