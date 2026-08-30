f6f417f43f97186bc21a8ee6dcfde178e0887606
Udaypratap Singh <uday2006is@gmail.com>
2026-08-30 10:04:43 -0700

    feat: add MemoryType enum

---
+++ src/main/java/com/medicalcontinuity/medicalcontinuity/enums/MemoryType.java
@@ -0,0 +1,12 @@
+package com.medicalcontinuity.medicalcontinuity.enums;
+
+public enum MemoryType {
+    CLINICAL_NOTE,
+    PATIENT_PREFERENCE,
+    FOLLOW_UP,
+    ALLERGY_ALERT,
+    CHRONIC_CONDITION,
+    FAMILY_HISTORY,
+    LIFESTYLE,
+    OTHER
+}
