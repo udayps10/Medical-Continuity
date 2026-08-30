e7ecf9041ab442dc441394d68dc9912d7db3797a
Udaypratap Singh <uday2006is@gmail.com>
2026-08-30 10:04:42 -0700

    feat: add DocumentType enum

---
+++ src/main/java/com/medicalcontinuity/medicalcontinuity/enums/DocumentType.java
@@ -0,0 +1,13 @@
+package com.medicalcontinuity.medicalcontinuity.enums;
+
+public enum DocumentType {
+    LAB_REPORT,
+    PRESCRIPTION,
+    IMAGING,
+    DISCHARGE_SUMMARY,
+    REFERRAL,
+    CONSENT_FORM,
+    IDENTIFICATION,
+    INSURANCE,
+    OTHER
+}
