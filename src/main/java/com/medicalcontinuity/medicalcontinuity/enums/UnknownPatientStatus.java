ddd71f378d58e658938fc038c530ea6c212784be
Udaypratap Singh <uday2006is@gmail.com>
2026-08-30 10:04:44 -0700

    feat: add UnknownPatientStatus enum

---
+++ src/main/java/com/medicalcontinuity/medicalcontinuity/enums/UnknownPatientStatus.java
@@ -0,0 +1,10 @@
+package com.medicalcontinuity.medicalcontinuity.enums;
+
+public enum UnknownPatientStatus {
+    UNIDENTIFIED,
+    UNDER_REVIEW,
+    MATCHED,
+    IDENTIFIED,
+    DISCHARGED,
+    DEceased
+}
