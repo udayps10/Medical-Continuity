05e22ebcfeade36abfdccd062594872fa3c14690
Udaypratap Singh <uday2006is@gmail.com>
2026-09-21 19:45:32 +0530

    updating

---
+++ backend/src/main/java/com/medicalcontinuity/medicalcontinuity/repositories/ Accessgrantrepository.java
@@ -0,0 +1,12 @@
+package com.drn.repository;
+
+import com.drn.entity.AccessGrant;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AccessGrantRepository extends JpaRepository<AccessGrant, Long> {
+    List<AccessGrant> findByPatientId(Long patientId);
+    List<AccessGrant> findByHospitalId(Long hospitalId);
+    List<AccessGrant> findByPatientIdAndStatus(Long patientId, AccessGrant.Status status);
+}
