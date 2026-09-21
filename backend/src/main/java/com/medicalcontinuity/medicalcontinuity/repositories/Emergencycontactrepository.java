6b5a0611b617ddf6cf2406895a690631ea82e3a1
Udaypratap Singh <uday2006is@gmail.com>
2026-09-21 19:48:39 +0530

    updating

---
+++ backend/src/main/java/com/medicalcontinuity/medicalcontinuity/repositories/Emergencycontactrepository.java
@@ -0,0 +1,10 @@
+package com.drn.repository;
+
+import com.drn.entity.EmergencyContact;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmergencyContactRepository extends JpaRepository<EmergencyContact, Long> {
+    List<EmergencyContact> findByPatientId(Long patientId);
+}
