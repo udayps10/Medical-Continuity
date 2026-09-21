51b14f7c19d7f7cbc1cc8c21e1a864669ecc0878
Udaypratap Singh <uday2006is@gmail.com>
2026-09-21 19:51:45 +0530

    updating

---
+++ backend/src/main/java/com/medicalcontinuity/medicalcontinuity/repositories/Documentchunkrepository.java
@@ -0,0 +1,10 @@
+package com.drn.repository;
+
+import com.drn.entity.DocumentChunk;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface DocumentChunkRepository extends JpaRepository<DocumentChunk, Long> {
+    List<DocumentChunk> findByDocumentIdOrderByChunkIndex(Long documentId);
+}
