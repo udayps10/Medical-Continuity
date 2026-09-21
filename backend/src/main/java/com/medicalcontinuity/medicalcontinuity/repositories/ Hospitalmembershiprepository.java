cc090f02df06275cad2df14650a26a5e0dd71428
Udaypratap Singh <uday2006is@gmail.com>
2026-09-21 19:44:54 +0530

    updating

---
+++ backend/src/main/java/com/medicalcontinuity/medicalcontinuity/repositories/ Hospitalmembershiprepository.java
@@ -0,0 +1,13 @@
+package com.drn.repository;
+
+import com.drn.entity.HospitalMembership;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface HospitalMembershipRepository extends JpaRepository<HospitalMembership, Long> {
+    List<HospitalMembership> findByUserId(Long userId);
+    List<HospitalMembership> findByHospitalId(Long hospitalId);
+    Optional<HospitalMembership> findByUserIdAndHospitalId(Long userId, Long hospitalId);
+}
